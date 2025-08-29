549fad36ada2c9916c5381b9ee7923950a08e592
aref <devfera14@gmail.com>
2025-08-29 15:10:49 -0300

    Initial Commit

---
+++ Formulario/FormularioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormularioApp.swift
+//  Formulario
+//
+//  Created by arefdeveloper on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FormularioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
